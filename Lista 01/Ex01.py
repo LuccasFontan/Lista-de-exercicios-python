d1000c9866448fb8079ac400ffa27601cde47254
LuccasFontan <Lucasfontan2003@gmail.com>
2025-08-06 14:55:36 -0300

    Update Ex01.py

--- Lista 01/Ex01.py
+++ Lista 01/Ex01.py
@@ -1,8 +1,15 @@
+# Exercício 01 - Faça um Programa que peça um número inteiro e determine se ele é PAR ou ÍMPAR. 
+# Dica: utilize o operador módulo (resto da divisão [%])
+
 try:
     numero = int(input("Digite um número inteiro: "))
+    
     if numero % 2 == 0:
         print("O número é PAR")
     else:
         print("O número é IMPAR")
 except ValueError:
-    print("Valor inválido, por favor digite um número inteiro")
+    print("Valor inválido, Por favor digite um número inteiro")
+    
+
+    
