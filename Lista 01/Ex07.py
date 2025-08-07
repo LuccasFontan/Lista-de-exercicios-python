1d97954b99f0b2ccdc2d46222b1eb7887acae51f
LuccasFontan <Lucasfontan2003@gmail.com>
2025-08-07 15:06:45 -0300

    Update Ex07.py

---
+++ Lista 01/Ex07.py
@@ -0,0 +1,11 @@
+# Exercicio 07 - Faça um Programa que faça 5 perguntas para pessoa sobre um crime. 
+# As perguntas são: 
+# A - "Telefonou para a vitima?"
+# B - "Esteve no local do crime?"
+# C - "Mora perto da vitima?"
+# D - "Devia para a vitima?"
+# E - "Já trabalhou para a vitima?"
+# O programa deve no final emitir uma classificaçao sobre a participaçao da pessoa no crime. 
+# Se a pessoa responder positivamente a 2 questoes ela deve ser classificada como "Suspeita",
+# entre 3 e 4 como "Cumplice" e 5 como "Assasino". Caso contrario, ele será classificado como "Inocente".
+
